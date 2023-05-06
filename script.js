22aeb4bb3b17f99f1024b89d28a5f9cd4c0758ae
anushalav_bhatt <bhattanushalav@gmail.com>
2023-05-07 00:36:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// "use strict"
+
+let result = () => {
+    let height = document.getElementById('heightfield').value;
+    let weight = document.getElementById('weightfield').value;
+    height = height / 100;
+    let bmi = (weight / (Math.pow(height,2)));
+    bmi = (Math.round(bmi));
+    document.getElementById('result').value = bmi;
+
+    document.getElementById('heightfield').value="";
+    document.getElementById('weightfield').value="";
+
+    if(bmi>0&&bmi<=18){
+        document.getElementById('totalbmi').innerHTML="UnderWeight";
+    }
+    else if(bmi>=18&&bmi<=25){
+        document.getElementById('totalbmi').innerHTML="Normal... YOU ARE HEALTY";
+    }
+    else if(bmi>25){
+        document.getElementById('totalbmi').innerHTML="OverWeight";
+    }
+}
+
