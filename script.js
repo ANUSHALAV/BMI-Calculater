722a0c87d2963d54df537067b7777b9fd9681263
Anushalav bhatt <bhattanushalav@gmail.com>
2023-09-05 12:59:58 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// "use strict"
+"use strict"
 
-let result = () => {
-    let height = document.getElementById('heightfield').value;
-    let weight = document.getElementById('weightfield').value;
+
+
+document.querySelector('#btn').addEventListener("click", (event) => {
+
+    event.preventDefault();
+    let height = parseInt(document.getElementById('heightfield').value);
+    let weight = parseInt(document.getElementById('weightfield').value);
+
+    
     height = height / 100;
-    let bmi = (weight / (Math.pow(height,2)));
+    let bmi = (weight / (Math.pow(height, 2)));
     bmi = (Math.round(bmi));
     document.getElementById('result').value = bmi;
 
-    document.getElementById('heightfield').value="";
-    document.getElementById('weightfield').value="";
+    document.getElementById('heightfield').value = "";
+    document.getElementById('weightfield').value = "";
 
-    if(bmi>0&&bmi<=18){
-        document.getElementById('totalbmi').innerHTML="UnderWeight";
+    if (bmi > 0 && bmi <= 18) {
+        document.getElementById('totalbmi').innerHTML = "UnderWeight";
     }
-    else if(bmi>=18&&bmi<=25){
-        document.getElementById('totalbmi').innerHTML="Normal... YOU ARE HEALTY";
+    else if (bmi >= 18 && bmi <= 25) {
+        document.getElementById('totalbmi').innerHTML = "Normal... YOU ARE HEALTY";
     }
-    else if(bmi>25){
-        document.getElementById('totalbmi').innerHTML="OverWeight";
+    else if (bmi > 25) {
+        document.getElementById('totalbmi').innerHTML = "OverWeight";
     }
-}
-
+});
